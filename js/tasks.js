24ae4aa8355bd52bf6c57612f1c10c535c2edc80
GabrielGorog <gorog1338@yahoo.com>
2020-02-12 19:29:16 +0200

    displaying tasks from API

--- js/tasks.js
+++ js/tasks.js
@@ -8,17 +8,37 @@ window.ToDoList = {
             method: "GET"
         }).done(function (response) {
             console.log(response);
+
+            ToDoList.displayTasks(JSON.parse (response));
         })
     },
 
     getTaskRow: function (task) {
+        //spread operator (...)
+
+        let formattedDeadline = new Date(...task.deadline).toLocaleDateString("ro");
+
+        // ternary oerator
+        let checkedAttribute = task.done ? " checked" : "";
+
         return `<tr>
             <td>${task.description}</td>
-            <td>${task.deadline}</td>
-            <td><input type="checkbox" data-id=${task.id} class="mark-done"/></td>
+            <td>${formattedDeadline}</td>
+            <td><input type="checkbox" data-id=${task.id} class="mark-done" ${checkedAttribute}/></td>
             <td><a href="#" data-id=${task.id} class="delete-task">
                 <i class="fas fa-trash-alt"></i>
         </tr>`
+    },
+
+    displayTasks: function (tasks) {
+        // weak-typed (javascript) vs strong-typed (java)
+
+        var tableBody = '';
+
+        tasks.forEach(task => tableBody += ToDoList.getTaskRow(task));
+
+        $("#tasks-table tbody").html(tableBody);
+
     }
 
 };
