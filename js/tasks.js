82fcc66d7438051fa55e3ca82e5562c3ed356b45
GabrielGorog <gorog1338@yahoo.com>
2020-02-12 21:05:36 +0200

    sending update task request to API

--- js/tasks.js
+++ js/tasks.js
@@ -13,6 +13,43 @@ window.ToDoList = {
         })
     },
 
+    createTask: function() {
+        let descriptionValue = $("#description-field").val();
+        let deadlineValue = $("#deadline-field").val();
+
+        let requestBody = {
+            description: descriptionValue,
+            deadline: deadlineValue
+        };
+
+        $.ajax({
+            url: ToDoList.API_BASE_URL,
+            method: "POST",
+            // also known as MIME type
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getTasks();
+        })
+    },
+
+    updateTask: function (id, done) {
+        let requestBody = {
+            done: done
+        };
+
+        $.ajax({
+            url: ToDoList.API_BASE_URL + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getTasks();
+        })
+    },
+
+
+
     getTaskRow: function (task) {
         //spread operator (...)
 
@@ -39,8 +76,33 @@ window.ToDoList = {
 
         $("#tasks-table tbody").html(tableBody);
 
+    },
+
+    bindEvents: function () {
+        // capturing the 'submit form' event to bind our function to it
+        $("#new-task-form").submit(function (event) {
+            event.preventDefault();
+
+            ToDoList.createTask();
+        });
+
+        // delegate is necessary here because the element .mark-done
+        // is not present in the page from the beginning, but injected later on
+        $("#tasks-table")
+            .delegate(".mark-done", "change", function(event) {
+
+                event.preventDefault();
+
+                // reading value of attributes prefixed with "data"
+                let taskId = $(this).data("id");
+                let checked = $(this).is(":checked");
+
+                ToDoList.updateTask(taskId, checked);
+        });
+
     }
 
 };
 
 ToDoList.getTasks();
+ToDoList.bindEvents();
